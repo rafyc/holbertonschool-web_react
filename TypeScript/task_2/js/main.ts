dfd59ec3c1e437bc3f8ac39730d9e022185a7340
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-23 16:13:57 +0200

    1

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -7,7 +7,7 @@ export interface DirectorInterface {
 export interface TeacherInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+    workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -33,7 +33,7 @@ class Teacher implements TeacherInterface {
         return 'Cannot have a break';
     }
 
-    workDirectorTasks(): string {
+    workTeacherTasks(): string {
         return 'Getting to work';
     }
 }
@@ -42,7 +42,21 @@ export interface CreateEmployee {
     salary: string | (number & { length: 8 })
 }
 
-function createEmployee(salary: string | number ): Director | Teacher {
+export const createEmployee = (firstName: string, lastName: string, salary: string | number ): DirectorInterface | TeacherInterface => {
     if (typeof salary === 'number' && salary < 500) return new Teacher();
     return new Director();
+}
+
+export const isDirector = (employee : DirectorInterface | TeacherInterface) : employee is DirectorInterface => {
+    return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
+
+export const executeWork = (employee : DirectorInterface | TeacherInterface) : string => {
+    return isDirector(employee) ? employee.workDirectorTasks() : employee.workTeacherTasks();
+}
+
+export type Subjects = 'Math' | 'History';
+
+export const teachClass = (todayClass : Subjects) : string => {
+    return todayClass === 'Math' ? 'Teaching Math': 'Teaching History'
 }
