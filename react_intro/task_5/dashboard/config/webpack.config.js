eeb0ba93d7f14ac3bbbb2af678cb68c88b3580ac
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-09-01 12:16:52 +0200

    5

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -16,4 +16,26 @@ module.exports = {
     open: true,
     hot: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
+    ],
+
+  },
 };
