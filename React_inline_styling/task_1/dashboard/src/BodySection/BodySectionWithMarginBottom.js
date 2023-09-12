17f4b39b0f8524eac19c4fff1468916a8a683ebc
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-09-12 18:35:57 +0200

    1

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import BodySction from './BodySection';
 import { PropTypes } from "prop-types";
+import { StyleSheet, css } from 'aphrodite';
 
 
 const BodySectionWithMarginBottom = (props) => {
 
   return (
-    <div className='bodySectionWithMargin'>
+    <div className={css(styles.cmarginBottom)}>
       <BodySction {...props} />
     </div>
   )
 }
-
+const styles = StyleSheet.create({
+  marginBottom: '40px'
+})
 BodySectionWithMarginBottom.propTypes = {
   children: PropTypes.node,
 }
