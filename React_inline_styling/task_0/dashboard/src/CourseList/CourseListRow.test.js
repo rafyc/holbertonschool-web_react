b8956450ae873a8e1baa1615465605aa64dd36b5
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-09-12 17:59:31 +0200

    0

--- React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,22 +1,30 @@
-import CourseListRow from "./CourseListRow";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from './CourseListRow';
 
-describe('When isHeader is true', () => {
-  it('renders one cell with colspan(2) when textSecondCell does not exist', () => {
-    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell={'Hello'} />);
-    expect(wrapper.find('th')).toHaveLength(1);
-  })
-  it('renders 2 cell when textSecondCell exist', () => {
-    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell={'Hello'} textSecondCell={'Holberton'} />);
-    expect(wrapper.find('th')).toHaveLength(2);
-  })
-})
+describe('CourseListRow component tests', () => {
 
-describe('When isHeader is not true', () => {
-  it('renders one cell with colspan(2) when textSecondCell does not exist', () => {
-    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell={'Hello'} textSecondCell={'Holberton'} />);
-    expect(wrapper.find('tr')).toHaveLength(1);
-    expect(wrapper.find('td')).toHaveLength(2);
-  })
+  it('renders CourseListRow component without crashing', () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="School" />);
+    expect(wrapper.exists()).toBe(true);
+  });
 
-})
+  it('renders when header is true and textSecondCell is null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="School" />);
+    const th = wrapper.find('th');
+    expect(th).toHaveLength(1);
+    expect(th.prop('colSpan')).toEqual(2);
+  });
+
+  it('renders when header is true and textSecondCell is not null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="School" textSecondCell="Year" />);
+    const th = wrapper.find('th');
+    expect(th).toHaveLength(2);
+  });
+
+  it('renders when header is false', () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="School" textSecondCell="Year" />);
+    const td = wrapper.find('td');
+    expect(td).toHaveLength(2);
+  });
+});
